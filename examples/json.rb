7f6fc5368ce816b5fa52ad2d9ddefca4a9105174
hiro-su <h.sugipon@gmail.com>
2013-08-07 19:08:53 +0900

    edit: examples/json.rb

--- examples/json.rb
+++ examples/json.rb
@@ -1,15 +1,15 @@
 require 'net/http'
 
 # in_http
-host, port = 'localhost', 7777
-path = "/test.tag"
-http = Net::HTTP.new(host, port)
-req = Net::HTTP::Post.new(path)
-req["Content-Type"] = "application/json"
-1.upto(10000) do |i|
-  req.body = "{\"key#{i}\":\"value#{i}\"}"
-  http.request(req)
-end
+#host, port = 'localhost', 7777
+#path = "/test.tag"
+#http = Net::HTTP.new(host, port)
+#req = Net::HTTP::Post.new(path)
+#req["Content-Type"] = "application/json"
+#1.upto(10000) do |i|
+#  req.body = "{\"key#{i}\":\"value#{i}\"}"
+#  http.request(req)
+#end
 
 # in_http_ex
 host, port = 'localhost', 8888
